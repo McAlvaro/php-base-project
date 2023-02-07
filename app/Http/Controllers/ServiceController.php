6e4d070b379ea05a5f0b5c81cdd1a29de569568c
McAlvaro <mc.alv641@gmail.com>
2023-02-06 21:36:14 -0400

    - Added Services Page - Added Contact Page

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Response;
+
+class ServiceController {
+
+    public function index() {
+
+        return new Response('service');
+
+    }
+
+}
---
+++ views/contact.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="text-center">
+        <h1>Contact</h1>
+        <div class="row w-50 mx-auto">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary float-end">Send</button>
+            </div>
+        </div>
+    </div>
+</div>
